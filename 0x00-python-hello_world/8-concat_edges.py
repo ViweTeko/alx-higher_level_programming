f94b4eb28abf65f68f1a60dbb281cdc16b52ab86
ViweTeko <iamviwe.teko@gmail.com>
2023-11-27 16:41:30 -0800

    Sorted concat edge

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-orientated programming\
         language that combines remarkable power with very clear syntax"
-str = str[39:69] + str[str.find("with"):str.find("with") + 5] + str[:6]
+str = str[39:69] + str[116:121] + str[:6]
 print(str)
