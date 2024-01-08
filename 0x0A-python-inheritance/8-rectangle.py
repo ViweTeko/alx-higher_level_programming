170e4d1d0b4f58565db5158c9fac8e0686c555c2
ViweTeko <iamviwe.teko@gmail.com>
2024-01-09 00:17:41 +0200

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module writes a class Rectangle that inherits from 
+Base Geometry 
+"""
+BaseGeometry = __import__('7-base_geometry.py').BaseGeometry
+class Rectangle(BaseGeometry):
+    """ This class creates a rectangle inherited from BaseGeometry
+    Attributes:
+        __init__(self) - Initializer
+        width - width of rectangle
+        height - height of rectangle
+    """
+    def __init__(self, width, height):
+        """ Initializes the instance"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" This module continues fro the previous file
+    Attributes:
+        __init__(self) - Initializes the object
+        width - The width of the rectangle
+        height - The height of the rectangle
+    """
+    def __init__(self, width, height):
+        """Initializes the instance"""
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+    def area(self):
+        """ Calculates the area of rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ Returns object in string format"""
+        return "[{}] {}/{}".format(self.__class__.__name,
+                self.__width, self.__height)
