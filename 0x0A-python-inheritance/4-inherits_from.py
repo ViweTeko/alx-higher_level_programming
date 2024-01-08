492cca5431e73bd19959faf67f6151f7ca5ab89d
ViweTeko <iamviwe.teko@gmail.com>
2024-01-08 23:33:53 +0200

    Inherits from

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" This module checks for inheritance """
+
+def inherits_from(obj, a_class):
+    """ Checks for inheritance of an object
+    Args:
+        obj: Object
+        a_class: Class
+    Return:
+        True or Flase
+    """
+    return type(obj) is not a_class and issubclass(type(obj), a_class)
