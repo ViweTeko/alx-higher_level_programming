5ec4a24074b5a98fc9dcc72d54f58b92e03a4bbe
ViweTeko <iamviwe.teko@gmail.com>
2024-01-08 22:53:09 +0200

    Same class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" Checks if its the same class """
+
+def is_same_class(obj, a_class):
+    """ Checks if object is the instance of given class
+    Args:
+        obj (any): Object
+        a_class (type): class to match
+    Returns:
+        True or False
+    """
+    if type(obj) == a_class:
+        return True
+    return False
