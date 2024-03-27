a1822189b7513d3f86647d7ebe3fb95c1c494950
ViweTeko <iamviwe.teko@gmail.com>
2024-03-27 15:58:57 +0200

    Python network advanced

---
+++ 0x10-python_network_0/100-status_code.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script sends request to URL (as args) and displays status code
+
+curl -sI -w '%{response_code}' "$1" -o /dev/null
---
+++ 0x10-python_network_0/101-post_json.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script sends JSON POST request
+
+curl -s -X POST "$1" -H "Content-Type: application/json" -d @"$2"
