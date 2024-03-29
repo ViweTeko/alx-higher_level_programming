bb383dc07e8a5045ee5f801edbcc554c366b16b0
ViweTeko <iamviwe.teko@gmail.com>
2024-03-29 21:18:43 +0200

    Better fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script makes a request to 0.0.0.0:5000/catch_me response "You got me!"
-curl -sL PUT -X -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,14 +8,13 @@ def find_peak(int_list):
         return None
     low = 0
     high = len(int_list)
-    mid = ((high - low) // 2) + low
-    mid = int(mid)
+    mid = int(((high - low) // 2) + low)
     if high == 1:
         return int_list[0]
     if high == 2:
         return max(int_list)
     if int_list[mid] >= int_list[mid - 1] and\
-                int_list[mid] >= int_list[mid + 1]:
+            int_list[mid] >= int_list[mid + 1]:
         return int_list[mid]
     if mid > 0 and int_list[mid] < int_list[mid + 1]:
         return find_peak(int_list[mid:])
